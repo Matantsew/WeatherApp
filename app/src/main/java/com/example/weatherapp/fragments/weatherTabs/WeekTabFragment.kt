5add4fc50a0fbd0352f1a5ec86f4274d629b0a12
dmytro <971397139Faster3>
2025-03-08 12:22:23 +0200

    viewModel init, fragments/items

--- app/src/main/java/com/example/weatherapp/fragments/weatherTabs/WeekTabFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/weatherTabs/WeekTabFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.weatherapp.databinding.FragmentWeekTabBinding
+import com.example.weatherapp.viewModels.MainViewModel
 
 class WeekTabFragment : Fragment() {
 
     private var _binding: FragmentWeekTabBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +25,9 @@ class WeekTabFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+    }
 }
